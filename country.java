1d75b43daae1b8fe3927bb14580cca0f3f371592
Austin <Diamondman5321@gmail.com>
2018-11-13 22:47:17 -0600

    Add files via upload

---
+++ country.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class country {
+    // Soldiers in region, who owns region
+    static int ss = 0, oP = 0;
+    
+    public static void claim(int p) {
+        
+    }
+    
+    // aTTACKING & dEFENDING
+    // Forces on both sides and registering which two sides are against eachother
+    public static void battle(int af, int df, int ap, int dp) {
+        
+    }
+}
