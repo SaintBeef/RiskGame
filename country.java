75503192f131b679ced12acc0c2945353636f09a
Austin Metz <Diamondman5321@gmail.com>
2018-11-15 05:48:26 -0600

    basis for regions?

--- country.java
+++ country.java
@@ -5,12 +5,15 @@ public class country {
     static int ss = 0, oP = 0;
     
     public static void claim(int p) {
-        
+        oP = p;
     }
     
     // aTTACKING & dEFENDING
     // Forces on both sides and registering which two sides are against eachother
     public static void battle(int af, int df, int ap, int dp) {
-        
+        af--;
+        int winner = 0, sum = af + df;
+        if (Math.random()*sum <= af)
+            claim(af);
     }
 }
